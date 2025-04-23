721715ee621ab2e3f336d91e51aa13b891a06159
Lingfeng Jin <jinlingfeng2579@gmail.com>
2025-04-23 21:42:13 +0200

    frontend deployment cors.go fixes

--- cmd/webapi/cors.go
+++ cmd/webapi/cors.go
@@ -17,7 +17,11 @@ func applyCORSHandler(h http.Handler) http.Handler {
 		}),
 		handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "DELETE", "PUT"}),
 		// Do not modify the CORS origin and max age, they are used in the evaluation.
-		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedOrigins([]string{
+			"https://wasa-text.vercel.app",
+			"https://wasa-text-git-main-lingfengs-projects-dd7a133a.vercel.app",
+			"https://wasa-text-befk2zdy4-lingfengs-projects-dd7a133a.vercel.app",
+			"*"}),
 		handlers.MaxAge(1),
 	)(h)
 }
